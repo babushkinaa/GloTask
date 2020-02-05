952e92c9aa183bca54d7871379400fe2e7bd5922
Alexey Babushkin <007-99@mail.ru>
2020-02-05 20:44:05 +0300

    основная домашка

--- js/app.js
+++ js/app.js
@@ -12,18 +12,7 @@ function findBook(bookItem) {
     let book = bookItem.textContent.trim().split('.'); // делаем массив
     return book[0].toString().slice(-1); // получаем номер книги
 }
-
-function removBook(elem,params) {
-    console.log(elem);
-    console.log(params);
-    console.log(params2);
-    for (let index = 0; index < params.length; index++) {
-        const element = elem[0];
-        element[0].removeChild(params[index]);
-    }
-    
-}
-for (let index = 0; index < book.length; index++) {
+for (let index = 0; index < book.length; index++) { // перебераем массив книг
     
     const element = findBook(book[index]);
     bookMassiv[element] = book[index];
@@ -31,8 +20,7 @@ for (let index = 0; index < book.length; index++) {
 }
 bookMassiv = bookMassiv.sort();
 let foo = (i) => books[0].removeChild(book[i]);
-let faa = function(i){books[0].appendChild(bookMassiv[i]);}
-
+let faa = (i)=> books[0].appendChild(bookMassiv[i]);
 
 for (let i = 0; i < book.length; i++) {
     
@@ -42,23 +30,54 @@ for (let i = 0; i < book.length; i++) {
    
     faa(i);
 }
-// let book3 = book[2].bookItem.textContent.trim().split('.');
 let num3;
-// book[3].getElementsByTagName('h2').firstChild.nodeValue;
-// console.log('nodeValue: ', book[3].getElementsByTagName('h2'));
 for (let index = 0; index < book.length; index++) {
     const element = findBook(book[index]);
     if(+element === 3){
        num3 = book[index].getElementsByTagName('h2');
-       book[index].getElementsByTagName('h2')[0] = 'Получится - "Книга 3. this и Прототипы Объектов"';
-       console.log(book[index]);
+       book[index].querySelector('a').textContent = '"Книга 3. this и Прототипы Объектов"';
     }
 }
-console.log(num3);
 
-console.log('bookMassiv: ', bookMassiv);
-console.log('bookMassiv: ', bookMassiv.sort());
-// Получится - "Книга 3. this и Прототипы Объектов"
+// книга 2
+bookGlava = book[0].querySelectorAll('li');
+bookGlava[0].textContent = 'Введение';
+bookGlava[1].textContent = 'Предисловие';
+bookGlava[2].textContent = 'Глава 1: Что такое область видимости?';
+bookGlava[3].textContent = 'Глава 2: Лексическая область видимости';
+bookGlava[4].textContent = 'Глава 3: Область видимости: функции против блоков';
+bookGlava[5].textContent = 'Глава 4: Поднятие переменных (Hoisting)';
+bookGlava[6].textContent = 'Глава 5: Замыкание области видимости';
+bookGlava[7].textContent = 'Приложение A: Динамическая область видимости';
+bookGlava[8].textContent = 'Приложение B: Полифиллинг блочной области видимости';
+bookGlava[9].textContent = 'Приложение D: Благодарности!';
+bookGlava[10].textContent = 'Приложение C: Лексический this';
+
+bookGlava5 = book[5].querySelectorAll('li');
+bookGlava5[0].textContent = 'Введение';
+bookGlava5[1].textContent = 'Предисловие';
+bookGlava5[2].textContent = 'Глава 1: Асинхронность: Сейчас и Тогда';
+bookGlava5[3].textContent = 'Глава 2: Колбеки';
+bookGlava5[4].textContent = 'Глава 3: Обещания';
+bookGlava5[5].textContent = 'Глава 4: Генераторы';
+bookGlava5[6].textContent = 'Глава 5: Производительность программы';
+bookGlava5[7].textContent = 'Глава 6: Бенчмаркинг и настройка';
+bookGlava5[8].textContent = 'Приложение A: Библиотека: asynquence';
+bookGlava5[9].textContent = 'Приложение B: Расширенные асинхронные шаблоны';
+bookGlava5[10].textContent = 'Приложение C: Благодарности!';
+
+
+
+
+bookGlava6ul = book[2].querySelectorAll('ul');
+bookGlava6 = book[2].querySelectorAll('li');
+let g8 = document.createElement('li');
+    g8.textContent = 'Глава 8: За пределами ES6';
+
+bookGlava6ul[0].insertBefore(g8,bookGlava6[9] )
+
+
+
 
 
 
